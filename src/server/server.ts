4d141ceeaaf3fc29b0c915eb9ea3467a4064ae20
Johnny Moreira <jmoreira@engebras.com.br>
2021-11-12 20:39:45 -0300

    Add prisma and model

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+app.get('*', function (req, res) {
+  res.status(404).send('what???')
+});
 
 export default app;
